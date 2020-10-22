6f192b0167762e834fd7af7f55cfeb051400b5ed
Cristian <ymvdeveloper@gmail.com>
2020-10-22 20:22:03 +0200

    Scalar op + dot_product + normalize()

--- Vector3/Vector3.cpp
+++ Vector3/Vector3.cpp
@@ -1,6 +1,7 @@
 #include "Vector3.h"
 
 template <class Tp>
+// Constructors
 Vector3<Tp>::Vector3()
 {
 	vec3[0] = 0.0;
@@ -23,4 +24,81 @@ Vector3<Tp>::Vector3(const Vector3<Tp>& v)
 	vec3[1] = v[1];
 	vec3[2] = v[2];
 }
+
+//scalar op
+
+template <class Tp>
+Vector3<Tp>& Vector3<Tp>::operator += (Tp v)
+{
+	vec3[0] += v;
+	vec3[1] += v;
+	vec3[2] += v;
+	return *this;
+}
+
+template <class Tp>
+Vector3<Tp>& Vector3<Tp>::operator -= (Tp v)
+{
+	vec3[0] -= v;
+	vec3[1] -= v;
+	vec3[2] -= v;
+	return *this;
+}
+
+template <class Tp>
+Vector3<Tp>& Vector3<Tp>::operator *= (Tp v)
+{
+	vec3[0] *= v;
+	vec3[1] *= v;
+	vec3[2] *= v;
+	return *this;
+}
+
+template <class Tp>
+Vector3<Tp>& Vector3<Tp>::operator /= (Tp v)
+{
+	vec3[0] /= v;
+	vec3[1] /= v;
+	vec3[2] /= v;
+	return *this;
+}
+
+
+
+//vector op
+
+template <class Tp>
+Vector3<Tp>& Vector3<Tp>::operator += (const Vector3<Tp>& v)
+{
+	vec3[0] += vec3[0];
+	vec3[1] += vec3[1];
+	vec3[2] += vec3[2];
+	return *this;
+}
+
+//math op
+template <class Tp>
+inline const Tp Vector3<Tp>::dot_product(const Vector3<Tp>& v) const
+{
+	return vec3[0] * v[0] + vec3[1] * v[1] + vec3[2] * v[2];
+}
+
+template <class Tp>
+inline const Tp Vector3<Tp>::norm() const
+{
+	return (Tp)sqrt(dot_product(*this));
+}
+
+template <class Tp>
+Vector3<Tp>& Vector3<Tp>::normalize()
+{
+	Tp n = norm();
+	if (n) {
+		vec3[0] /= n;
+		vec3[1] /= n;
+		vec3[2] /= n;
+	}
+	return *this;
+}
+
 
--- Vector3/Vector3.h
+++ Vector3/Vector3.h
@@ -8,21 +8,24 @@ public:
 	Vector3(const Tp x, const Tp y, const Tp z);
 	Vector3(const Vector3<Tp>& v);
 
+	//math op
 	Vector3<Tp>& normalize();
-	const Tp distance_to(const Vector3<Tp>&) const;
+	const float distance_to(const Vector3<Tp>&) const;
 	const Tp dot_product(const Vector3<Tp>&) const;
 	const Vector3<T> cross_product(const Vector3<T>&) const;
-	const Tp angle_between(const Vector3<Tp>&) const;
+	const float angle_between(const Vector3<Tp>&) const;
 	
 	//scalar op
 	Vector3<Tp>& operator+=(const Tp s);                  
 	Vector3<Tp>& operator-=(const Tp s);                  
 	Vector3<Tp>& operator*=(const Tp s);
+	Vector3<Tp>& operator/=(const Tp s);
 
 	//vector op
-	Vector3<Tp>& operator+=(const vector3<Tp>& v);        
-	Vector3<Tp>& operator-=(const vector3<Tp>& v);        
-	Vector3<Tp>& operator*=(const vector3<Tp>& v);        
+	Vector3<Tp>& operator+=(const Vector3<Tp>& v);
+	/*Vector3<Tp>& operator-=(const vector3<Tp>& v);        
+	Vector3<Tp>& operator*=(const vector3<Tp>& v);*/        
 private:
+	const Tp norm() const;
 	Tp vec3[3];
 };
